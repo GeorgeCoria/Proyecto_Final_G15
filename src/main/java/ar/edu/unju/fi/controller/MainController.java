4b59ee5c546c6252b56ca1a4c7d9b91cc78c63e0
Toconas, Juan <@>
2020-06-15 12:12:48 -0300

    Asunto : Creado controlador basico para probar paginas y base de Registrador.
    Detalles : Se crea controlador basico para probar footer.

---
+++ src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.fi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+
+	@RequestMapping("/registrador")
+	public String main(Model model) {
+		return "registrador";
+	}
+
+}
