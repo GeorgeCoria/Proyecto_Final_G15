9c4171d4fbe90616ee03ab50a2c74d4901f327d7
Castillo, Jorge Emanuel <@>
2020-06-16 19:44:01 -0300

    Asunto: Creacion de una nueva plantilla y actualizacion.
    1) Se agrega la plantilla consultas.html: Se desarrollo los 3 esqueleto
    para las consultas de (localidad-patente-tripulante).
    2) Se agrega la archivo consultas.css
    3) Actualiacion de clase MainController para la plantilla actual.
    4) Falta documentar toda la plantilla, falta darle estilo a patente y
    tripulante.

--- src/main/java/ar/edu/unju/fi/controller/MainController.java
+++ src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -2,7 +2,10 @@ package ar.edu.unju.fi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
 
 @Controller
 public class MainController {
@@ -17,5 +20,9 @@ public class MainController {
 	public String login() {
 		return "login";
 	}
-
+	
+	@GetMapping("/consultas")
+	public String getList() {
+		return "consultas";
+	}
 }
