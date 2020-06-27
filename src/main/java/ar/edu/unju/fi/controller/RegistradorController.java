3200a14e30f091f02d0441ad496712a8f69ef8b0
Toconas, Juan <@>
2020-06-27 14:14:59 -0300

    Asunto : Se agrega vista basica para registrador
    Descripcion:Se tiene muchos problemas con el git que dice que dice que tengo modificaciones en todo el documento, voy a hacer commits muy pequeños para no eliminar nada que al parecer lo hice sin querer, por favor tener una copia de todo por separado.

---
+++ src/main/java/ar/edu/unju/fi/controller/RegistradorController.java
@@ -0,0 +1,33 @@
+/**
+ * @author Velarde Marcia
+ * 
+ */
+
+package ar.edu.unju.fi.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ar.edu.unju.fi.service.ILocalidadService;
+
+@Controller
+public class RegistradorController {
+	
+	@Autowired
+	private ILocalidadService localidadService;
+	
+	@GetMapping("/registrador")
+	public String registradorVehiculo(Model model) {
+		model.addAttribute("listaLocalidades", localidadService.listarLocalidades());
+		return "registradorVehiculos";
+	}
+	
+	@GetMapping("/regvis")
+	public String registradorVistas(Model model) {
+		model.addAttribute("listaLocalidades", localidadService.listarLocalidades());
+		return "registradorVista";
+	}
+
+}
