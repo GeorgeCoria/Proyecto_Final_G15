86633e3429db3d556817d2466539f2e6dac20f3f
Toconas, Juan <@>
2020-07-08 15:25:18 -0300

    Asunto : Se agrega documentacion a MainController

--- src/main/java/ar/edu/unju/fi/controller/MainController.java
+++ src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -1,5 +1,12 @@
 package ar.edu.unju.fi.controller;
 
+/**
+ * Clase que se encargada de tomar las peticiones y controlar que respuesta debe
+ * ser presentada en la vista (paginas html: adminPrincipal.html,
+ * adminFormulario.html) de nuestro proyecto.
+ */
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,17 +14,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	
-	
+	/**
+	 * Muestra la pagina login ante la peticion /
+	 * @return la pagina de login
+	 */
 	@GetMapping("/")
 	public String logIn() {
 		return "login2";
 	}
 	
+	/**
+	 * Muestra la pagina login ante la peticion /login
+	 * @return la pagina de login
+	 */
 	@RequestMapping("/login")
 	public String login() {
 		return "login2";
 	}
 	
+	/**
+	 * Muestra la pagina sinPermisos usada por webSecurityConfig ante la peticion /sinPermisos  
+	 * @return pagina sin permisos
+	 */
 	@RequestMapping("/sinPermisos")
 	public String sinPermisos() {
 		return "sinPermisos";
